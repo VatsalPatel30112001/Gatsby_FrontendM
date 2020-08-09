6ec31a180650c74fe5e9bd3a350bac5d6c76b231
rp7275 <17dit015@charusat.edu.in>
2020-08-09 16:42:34 +0530

    commit 2

---
+++ graphql-data/gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+
+  siteMetadata: {
+    title: 'Welcome to Pandas World'
+  },
+
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
