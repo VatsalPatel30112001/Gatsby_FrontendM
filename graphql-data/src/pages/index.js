07f44d3b64bb01556a4fc47c6903b0a72a91e555
VatsalPatel30112001 <71972507+VatsalPatel30112001@users.noreply.github.com>
2020-10-01 16:55:02 +0530

    Update index.js

--- graphql-data/src/pages/index.js
+++ graphql-data/src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <div>
         <img
           src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-          alt="Group of pandas eating bamboo"
+          alt="try to reload"
         />
       </div>
     </Layout>
